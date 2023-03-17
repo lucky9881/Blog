ed5f0469723ac181db6d2797749091e6df375c34
lucky9881 <58693862+lucky9881@users.noreply.github.com>
2023-03-17 17:43:25 +0530

    Delete home directory

--- home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from home.models import Contact
-
-# Register your models here.
-
-class ContactAdmin(admin.ModelAdmin):
-    list_display=('name','email','phone','msg','date')
-
-admin.site.register(Contact,ContactAdmin)
